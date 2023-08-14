6af5df429c23c305a7896ddf8c6fcf8f6c21ea68
unknown <patiladityaajit@gmail.com>
2023-08-14 16:51:28 +0530

    updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,103 +1,83 @@
-import { Card, CardBody, Stack, Text, Flex, Icon, Avatar, AvatarBadge, } from "@chakra-ui/react";
+import { Card, CardBody, Stack, Text, Flex, Icon, Avatar, AvatarBadge } from "@chakra-ui/react";
 import { SettingsIcon } from "@chakra-ui/icons";
 
 export default function KbCard({ cardData, userData, avatar }) {
-    const user = userData.find((user) => user.id === cardData.userId);
+  const user = userData.find((user) => user.id === cardData.userId);
 
-    return (
-          <Card direction={{ base: "column", sm: "row" }} overflow="hidden" variant="outline" w={['88vw','350px']} >
-
-                <CardBody p={2}>
-                      <Stack>
-                            <Flex justify={"space-between"} fontSize={12} color="gray" >
-                                  <div>{cardData.id}</div>
-                                  <div>
-                                        {avatar ? (
-                                              <Avatar
-                                                    name={user.name}
-                                                    w={5}
-                                                    mr={2}
-                                                    h={5}
-                                                    size={"xs"}
-                                              >
-                                                    {user.available ? (
-                                                          <AvatarBadge
-                                                                boxSize="1em"
-                                                                bg="yellow.400"
-                                                          />
-                                                    ) : (
-                                                          <AvatarBadge
-                                                                boxSize="1em"
-                                                                bg="gray.400"
-                                                          />
-                                                    )}
-                                              </Avatar>
-                                        ) : (
-                                              ""
-                                        )}
-                                  </div>
-                            </Flex>
-                      </Stack>
-
-                      <Stack p={0}>
-                            <Text
-                                  fontSize={15}
-                                  align={"left"}
-                                  fontWeight={"500"}
-                            >
-                                  {cardData.title}
-                            </Text>
-                      </Stack>
-
-                      <Stack py={2}>
-                            <Flex alignItems={"center"}>
-                                  <SettingsIcon
-                                        border="1px"
-                                        borderColor="gray.200"
-                                        borderWidth={1}
-                                        w={6}
-                                        h={"22px"}
-                                        p={"3px"}
-                                        borderRadius={3}
-                                  ></SettingsIcon>
-                                  {cardData.tag.map((item) => (
-                                        <Flex
-                                              align={"left"}
-                                              border="1px"
-                                              borderColor="gray.200"
-                                              borderWidth={1}
-                                              borderRadius={5}
-                                              ml={2}
-                                              px={1}
-                                              alignItems={"center"}
-                                              h="22px"
-                                              alignSelf={"center"}
-                                        >
-                                              <Icon
-                                                    viewBox="0 0 200 200"
-                                                    color="gray.400"
-                                                    w={3}
-                                                    mr={1}
-                                                    alignItems={"center"}
-                                              >
-                                                    <path
-                                                          fill="currentColor"
-                                                          d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
-                                                    />
-                                              </Icon>
-                                              <Flex>
-                                                    <Text
-                                                          fontSize={14}
-                                                          color="gray.600"
-                                                    >
-                                                          {item}
-                                                    </Text>
-                                              </Flex>
-                                        </Flex>
-                                  ))}
-                            </Flex>
-                      </Stack>
-                </CardBody>
-          </Card>
-    );
+  return (
+    <Card
+      direction={{ base: "column", sm: "row" }}
+      overflow="hidden"
+      variant="outline"
+      w="100%"
+      mb={3}
+    >
+      <CardBody p={2}>
+        <Stack spacing={1}>
+          <Flex justify={"space-between"} fontSize={{ base: "10px", sm: "12px" }} color="gray">
+            <div>{cardData.id}</div>
+            <div>
+              {avatar ? (
+                <Avatar name={user.name} w={5} mr={2} h={5} size={"xs"}>
+                  {user.available ? (
+                    <AvatarBadge boxSize="1em" bg="yellow.400" />
+                  ) : (
+                    <AvatarBadge boxSize="1em" bg="gray.400" />
+                  )}
+                </Avatar>
+              ) : (
+                ""
+              )}
+            </div>
+          </Flex>
+          <Text fontSize={{ base: "13px", sm: "15px" }} align={"left"} fontWeight={"500"}>
+            {cardData.title}
+          </Text>
+          <Flex flexWrap="wrap" alignItems={"center"}>
+            <SettingsIcon
+              border="1px"
+              borderColor="gray.200"
+              borderWidth={1}
+              w={6}
+              h={"22px"}
+              p={"3px"}
+              borderRadius={3}
+            ></SettingsIcon>
+            {cardData.tag.map((item) => (
+              <Flex
+                align={"left"}
+                border="1px"
+                borderColor="gray.200"
+                borderWidth={1}
+                borderRadius={5}
+                ml={2}
+                px={1}
+                alignItems={"center"}
+                h="22px"
+                alignSelf={"center"}
+              >
+                <Icon
+                  viewBox="0 0 200 200"
+                  color="gray.400"
+                  w={{ base: "2", sm: "3" }}
+                  mr={1}
+                  alignItems={"center"}
+                >
+                  <path
+                    fill="currentColor"
+                    d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+                  />
+                </Icon>
+                <Flex>
+                  <Text fontSize={{ base: "12px", sm: "14px" }} color="gray.600">
+                    {item}
+                  </Text>
+                </Flex>
+              </Flex>
+            ))}
+          </Flex>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
 }
--- src/components/Kanbanboard.js
+++ src/components/Kanbanboard.js
@@ -1,121 +1,95 @@
 import KbCard from "../components/Card";
-
 import { Flex, Stack, Heading, Text } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 import Icons from "./Icons";
-const priorityLabels = {
-      0: "No priority",
-      1: "Low",
-      2: "Medium",
-      3: "High",
-      4: "Urgent",
-};
-const priorityColumns = ["0", "1", "2", "3", "4"];
 
+const priorityLabels = {
+  0: "No priority",
+  1: "Low",
+  2: "Medium",
+  3: "High",
+  4: "Urgent",
+};
+
+const priorityColumns = ["0", "1", "2", "3", "4"];
 const statusColumns = ["Todo", "In progress", "Backlog", "Done", "Canceled"];
+
 export default function Board({ sortedGroups, users, selectedGrouping }) {
-      if (selectedGrouping === "status" || selectedGrouping === "priority") {
-            const myArr =
-                  selectedGrouping === "status"
-                        ? statusColumns
-                        : priorityColumns;
-            return (
-                  <Flex bg={"gray.50"} direction={["column", "row"]}>
-                        {myArr.map((column) => (
-                              <Stack key={column} m={3} w={['88vw',350]}>
-                                    <Flex
-                                          justify={"space-between"}
-                                          alignItems={"center"}
-                                    >
-                                          <Flex alignItems={"center"}>
-                                                <Icons
-                                                      groupKey={column}
-                                                      userData={users}
-                                                />
-                                                <Text
-                                                      fontWeight={500}
-                                                      fontSize={"13px"}
-                                                      textAlign={"left"}
-                                                      mr={"2"}
-                                                >
-                                                      {selectedGrouping ===
-                                                      "status"
-                                                            ? column
-                                                            : priorityLabels[
-                                                                    column
-                                                              ]}
-                                                </Text>
-                                                <Text
-                                                      fontSize={"13px"}
-                                                      color={"gray.500"}
-                                                >
-                                                      {
-                                                            sortedGroups.find(
-                                                                  (group) =>
-                                                                        group.groupKey ===
-                                                                        column
-                                                            )?.items.length
-                                                      }
-                                                </Text>
-                                          </Flex>
-                                          <AddIcon w={3} />
-                                    </Flex>
-                                    {sortedGroups
-                                          .find(
-                                                (group) =>
-                                                      group.groupKey === column
-                                          )
-                                          ?.items.map((item) => (
-                                                <KbCard
-                                                      key={item.id}
-                                                      cardData={item}
-                                                      userData={users}
-                                                      avatar={true}
-                                                />
-                                          ))}
-                              </Stack>
-                        ))}
-                  </Flex>
-            );
-      } else {
-            return (
-                  <Flex bg={"gray.50"} direction={["column", "row"]}>
-                        {sortedGroups.map(({ groupKey, items }) => (
-                              <Stack key={groupKey} m={3}>
-                                    <Flex
-                                          justify={"space-between"}
-                                          alignItems={"center"}
-                                    >
-                                          <Flex alignItems={"center"}>
-                                                <Icons
-                                                      groupKey={groupKey}
-                                                      userData={users}
-                                                />
-                                                <Text
-                                                      fontWeight={500}
-                                                      fontSize={"13px"}
-                                                      textAlign={"left"}
-                                                      mr={"2"}
-                                                >
-                                                      {groupKey}
-                                                </Text>
-                                                <Text fontSize={"13px"}>
-                                                      {items.length}
-                                                </Text>
-                                          </Flex>
-                                          <AddIcon w={3} />
-                                    </Flex>
-                                    {items.map((item) => (
-                                          <KbCard
-                                                key={item.id}
-                                                cardData={item}
-                                                userData={users}
-                                                avatar={false}
-                                          />
-                                    ))}
-                              </Stack>
-                        ))}
-                  </Flex>
-            );
-      }
+  if (selectedGrouping === "status" || selectedGrouping === "priority") {
+    const myArr =
+      selectedGrouping === "status" ? statusColumns : priorityColumns;
+    return (
+      <Flex bg={"gray.50"} direction={{ base: "column", md: "row" }}>
+        {myArr.map((column) => (
+          <Stack key={column} m={3} w={{ base: "100%", sm: "88vw", md: 350 }}>
+            <Flex justify={"space-between"} alignItems={"center"}>
+              <Flex alignItems={"center"}>
+                <Icons groupKey={column} userData={users} />
+                <Text
+                  fontWeight={500}
+                  fontSize={{ base: "12px", md: "13px" }}
+                  textAlign={"left"}
+                  mr={"2"}
+                >
+                  {selectedGrouping === "status"
+                    ? column
+                    : priorityLabels[column]}
+                </Text>
+                <Text fontSize={{ base: "12px", md: "13px" }} color={"gray.500"}>
+                  {
+                    sortedGroups.find(
+                      (group) => group.groupKey === column
+                    )?.items.length
+                  }
+                </Text>
+              </Flex>
+              <AddIcon w={3} />
+            </Flex>
+            {sortedGroups
+              .find((group) => group.groupKey === column)
+              ?.items.map((item) => (
+                <KbCard
+                  key={item.id}
+                  cardData={item}
+                  userData={users}
+                  avatar={true}
+                />
+              ))}
+          </Stack>
+        ))}
+      </Flex>
+    );
+  } else {
+    return (
+      <Flex bg={"gray.50"} direction={{ base: "column", md: "row" }}>
+        {sortedGroups.map(({ groupKey, items }) => (
+          <Stack key={groupKey} m={3} w={{ base: "100%", sm: "88vw", md: 350 }}>
+            <Flex justify={"space-between"} alignItems={"center"}>
+              <Flex alignItems={"center"}>
+                <Icons groupKey={groupKey} userData={users} />
+                <Text
+                  fontWeight={500}
+                  fontSize={{ base: "12px", md: "13px" }}
+                  textAlign={"left"}
+                  mr={"2"}
+                >
+                  {groupKey}
+                </Text>
+                <Text fontSize={{ base: "12px", md: "13px" }}>{items.length}</Text>
+              </Flex>
+              <AddIcon w={3} />
+            </Flex>
+            {items.map((item) => (
+              <KbCard
+                key={item.id}
+                cardData={item}
+                userData={users}
+                avatar={false}
+              />
+            ))}
+          </Stack>
+        ))}
+      </Flex>
+    );
+  }
 }
